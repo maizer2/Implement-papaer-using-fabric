cd3b43829746a6e8a70c0f972bd5b8c90ef1bfc4
maizer2 <jhw3979@gmail.com>
2023-05-25 13:18:46 +0000

    add

--- models/diffusion/Diffusion.py
+++ models/diffusion/Diffusion.py
@@ -41,18 +41,8 @@ class DDPM(nn.Module):
         
         self.n_steps = n_steps
         self.n_samples = n_samples
-        
+    
         self.beta = torch.linspace(0.0001, 0.02, n_steps)
-        self.alpha = 1. - self.beta
-        self.alpha_bar = torch.cumprod(self.alpha, 0)
-        self.sigma2 = self.beta
-    
-    
-    def set_variable_device(self, device):
-        self.beta = self.beta.to(device)
-        self.alpha = self.alpha.to(device)
-        self.alpha_bar = self.alpha_bar.to(device)
-        self.sigma2 = self.sigma2.to(device)
         
         
     def gather(self, consts, t):
@@ -61,31 +51,34 @@ class DDPM(nn.Module):
         return c.reshape(-1, 1, 1, 1)
     
     
-    def q_xt_x0(self, x0, t):
-        mean = torch.sqrt(self.gather(self.alpha_bar, t)) * x0
-        var = 1 - self.gather(self.alpha_bar, t)
+    def q_xt_x0(self, x0, t, alpha_bar):
+        mean = torch.sqrt(self.gather(alpha_bar, t)) * x0
+        var = 1 - self.gather(alpha_bar, t)
         return mean, var
     
     
-    def q_sample(self, x0, t, eps):            
-        mean, var = self.q_xt_x0(x0, t)
+    def q_sample(self, x0, t, eps, alpha_bar):            
+        mean, var = self.q_xt_x0(x0, t, alpha_bar)
         
         return mean + (torch.sqrt(var)) * eps
     
     
-    def p_sample(self, xt, t):
+    def p_sample(self, xt, t, alpha):
+        alpha_bar = torch.cumprod(alpha, 0)
+        sigma2 = self.beta
+        
         eps_theta = self.eps_model(xt, t)
         
-        alpha = self.gather(self.alpha, t)
+        alpha = self.gather(alpha, t)
         beta = 1 - alpha
         
-        alpha_bar = self.gather(self.alpha_bar, t)
+        alpha_bar = self.gather(alpha_bar, t)
         beta_bar = 1 - alpha_bar
         
         eps_coef = torch.div(beta, torch.sqrt(beta_bar))
         
         mean = torch.div(1, torch.sqrt(alpha)) * (xt - eps_coef * eps_theta)
-        var = self.gather(self.sigma2, t)
+        var = self.gather(sigma2, t)
         
         eps = torch.randn_like(xt, device=xt.device)
         
@@ -93,26 +86,33 @@ class DDPM(nn.Module):
     
     
     def forward(self, z):
+        self.beta = self.beta.to(z.device)
+        alpha = 1. - self.beta
+        
         with torch.no_grad():
             # Remove noise for $T$ steps
             for t_ in range(self.n_steps):
                 # $t$
                 t = self.n_steps - t_ - 1
+                t = z.new_full((self.n_samples,), t, dtype=torch.long, device=z.device)
                 # Sample from $\textcolor{lightgreen}{p_\theta}(x_{t-1}|x_t)$
-                x = self.p_sample(z, z.new_full((self.n_samples,), t, dtype=torch.long, device=z.device))
+                z = self.p_sample(z, t, alpha)
         
-        return x
+        return z
     
     
     def get_loss(self, batch, epoch):
+        
         x0, _ = batch
+        self.beta = self.beta.to(x0.device)
+        alpha = 1. - self.beta
+        alpha_bar = torch.cumprod(alpha, 0)
         
         t = torch.randint(0, self.n_steps, (x0.size(0), ), device=x0.device, dtype=torch.long)
         noise = torch.randn_like(x0, device=x0.device)
         
-        xt = self.q_sample(x0.contiguous(), t.contiguous(), noise.contiguous())
-        eps_theta = self.eps_model(xt.contiguous(), t.contiguous())
-        
+        xt = self.q_sample(x0, t, noise, alpha_bar)
+        eps_theta = self.eps_model(xt, t)
         loss = self.criterion(noise, eps_theta)
 
         return loss
@@ -148,12 +148,12 @@ class LitDiffusion(pl.LightningModule):
         
         
     def configure_optimizers(self):
-        optim = self.optimizer(self.model.parameters(), self.lr)
+        optim = self.optimizer(self.model.eps_model.parameters(), self.lr)
         return optim
     
     
     def training_step(self, batch, batch_idx):
-        self.model.set_variable_device(self.device)
+        # self.model.set_variable_device(self.device)
         loss = self.model.get_loss(batch, self.current_epoch)
         self.log("train_loss", loss, prog_bar=True, sync_dist=True)
         return loss
@@ -167,13 +167,13 @@ class LitDiffusion(pl.LightningModule):
     
     
     def validation_step(self, batch, batch_idx):
-        self.model.set_variable_device(self.device)
+        # self.model.set_variable_device(self.device)
         loss = self.model.get_loss(batch, self.current_epoch)
         self.log("val_loss", loss, prog_bar=True, sync_dist=True)
         return loss
     
     def test_step(self, batch, batch_idx):
-        self.model.set_variable_device(self.device)
+        # self.model.set_variable_device(self.device)
         loss = self.model.get_loss(batch, self.current_epoch)
         self.log("test_loss", loss, prog_bar=True, sync_dist=True)
         return loss
